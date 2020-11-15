15d7f7ed850b1bebf8f4042e3b7293168fafb6c1
backstab319 <dev.siddhartha.gupta@gmail.com>
2020-11-16 00:13:43 +0530

    Added snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,17 @@ export class AppComponent implements OnInit {
   public slideVal: string = String();
   public toggleForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.toggleForm = this.fb.group({
       toggle: ['']
     });
-
     this.toggleForm.valueChanges.subscribe(val => this.toggleHandler(val));
   }
 
-  public toggleHandler = (val: string): void => {
+  public toggleHandler = (val: any): void => {
+    this.snackBar.open(`The value is ${val.toggle}`, 'Ok');
     console.log(val);
   };
 }
